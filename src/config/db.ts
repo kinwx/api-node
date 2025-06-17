cb90f495d40e00bbbbd8ed6b78b24b416f9e6b23
kinwx <davimessias26@gmail.com>
2025-06-17 10:08:57 -0300

    refactor: atualizar variáveis de ambiente e melhorar a configuração do Sequelize

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,24 +1,26 @@
-import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
+import { Dialect, Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASSWORD, {
+  process.env.DB_DATABASE as string,
+  process.env.DB_USERNAME as string,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    dialect: process.env.DB_CONNECTION as Dialect,
     logging: false,
-});
+  }
+);
 
 try {
-    sequelize.authenticate().then(() => {
-        console.log("Banco de dados conectado.");
-    });
+  sequelize.authenticate().then(() => {
+    console.log("Banco de dados conectado.");
+  });
 } catch (error) {
-    console.warn("Error ao conectar com o banco de dados.");
-    console.error(error);
-};
+  console.warn("Error ao conectar com o banco de dados.");
+  console.error(error);
+}
 
 export default sequelize;
